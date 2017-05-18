36d68e2f4c07b5ea9b7278be8c40fc0c89bd6908
conor <conor0456@gmail.com>
2017-05-18 04:02:07 -0700

    First commit

---
+++ darcyInstallScript.sh
@@ -0,0 +1,6 @@
+#/usr/bin/sh
+#sudo add-apt-repository -y ppa:dawidd0811/neofetch ppa:noobslab/icons ppa:teejee2008/ppa
+
+sudo apt update
+sudo apt install -y vim ssh conky-all apache2 r-base deluge neofetch 
+neofetch dalisha-icons unity-tweak-tool conky-manager git
