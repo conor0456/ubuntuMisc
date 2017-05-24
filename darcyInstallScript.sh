a66eea1e49c16883ce79c29bf4fcf61009349637
conor <conor0456@gmail.com>
2017-05-23 22:44:57 -0700

    Added background image

--- darcyInstallScript.sh
+++ darcyInstallScript.sh
@@ -1,6 +1,7 @@
 #/usr/bin/sh
 #sudo add-apt-repository -y ppa:dawidd0811/neofetch ppa:noobslab/icons ppa:teejee2008/ppa
+#sudo add-apt-repository -y ppa:webupd8team/atom
 
 sudo apt update
 sudo apt install -y vim ssh conky-all apache2 r-base deluge neofetch 
-neofetch dalisha-icons unity-tweak-tool conky-manager git
+neofetch dalisha-icons unity-tweak-tool conky-manager git atom
